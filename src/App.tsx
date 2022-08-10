c28916f4ce348926b44362e4a8a7474f2c0d670c
Mateus Herculano <mateuherculano0@gmail.com>
2022-08-10 16:29:01 -0300

    feat: css global / fonts / header

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { v4 as uuid } from 'uuid';
+import { PlusCircle } from 'phosphor-react';
+
+import { Header } from './components/Header';
+
+import styles from './App.module.css'
+
+import './global.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [tasks, setTasks] = useState([
+    {
+      id: uuid(),
+      title: 'Terminar o desafio',
+      isComplete: true,
+    },
+    {
+      id: uuid(),
+      title: 'Terminar trilha de node',
+      isComplete: false,
+    },
+    {
+      id: uuid(),
+      title: 'Terminar trilha de react native',
+      isComplete: false,
+    },
+
+  ]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+
+        <form className={styles.todoForm}>
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+
+          <button type="submit">
+            Criar
+            <PlusCircle size={24} />
+          </button>
+        </form>
+
+        <section>
+          <header>
+            <div className={styles.createdTasks}>
+              <strong>Tarefas criadas</strong>
+              <span>5</span>
+            </div>
+            <div className={styles.completedTasks}>
+              <strong>Concluídas</strong>
+              <span>2 de 5</span>
+            </div>
+          </header>
+
+          <main>
+
+          </main>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
